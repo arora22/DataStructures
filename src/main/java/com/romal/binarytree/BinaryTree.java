1847bfa18528d8116325f6fd87b22372dc3bb006
rarora <arora_romal@rediffmail.com>
2019-06-14 09:36:01 +0530

    Adding Binary tree cl with empty helper methods

---
+++ src/main/java/com/romal/binarytree/BinaryTree.java
@@ -0,0 +1,17 @@
+package com.romal.binarytree;
+
+public class BinaryTree {
+
+   private TreeNode root;
+
+   public void insert(Integer data) {
+   }
+
+   public TreeNode find(Integer data) {
+      return null;
+   }
+
+   public void delete(Integer data) {
+   }
+
+}
